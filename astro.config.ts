56bbc4706539be4bbb83502769cf4049599d8e66
Roman <dev@rman.dev>
2026-01-10 13:55:35 +0000

    wip

--- astro.config.ts
+++ astro.config.ts
@@ -1,3 +1,8 @@
+import solid from "@astrojs/solid-js";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
-export default defineConfig({});
+export default defineConfig({
+  integrations: [solid()],
+  vite: { plugins: [tailwindcss()] },
+});
