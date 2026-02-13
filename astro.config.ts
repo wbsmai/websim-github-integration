092ed6143781459492d23493bc8ba92d53bcfa1f
Roman <dev@rman.dev>
2026-02-13 20:46:51 +0000

    cf adapter

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,11 @@
+import cloudflare from "@astrojs/cloudflare";
 import solid from "@astrojs/solid-js";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+  output: "server",
+  adapter: cloudflare(),
   integrations: [solid()],
   vite: { plugins: [tailwindcss()] },
   site: "https://websim.rman.dev",
